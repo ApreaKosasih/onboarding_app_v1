81c5070a562c862f6f55f050b9c4867dbb994dd4
Aprea <apreak@gmail.com>
2021-03-22 15:19:53 +0700

    change value of text

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -37,8 +37,9 @@ List<SliderModel> getSlide() {
 
   //1
   sliderModel.setImgAssetsPath("assets/bg1.png");
-  sliderModel.setTitle("KuliahOnline");
-  sliderModel.setDesc("loremipsum");
+  sliderModel.setTitle("Temukan\nKampus Ideal");
+  sliderModel.setDesc(
+      "Informasi mengenai rekomendasi kampus (PTS) terbaik di seluruh wilayah Indonesia dengan berbagai pilihan kelas dan program studi favorit.");
 
   slides.add(sliderModel);
 
@@ -46,8 +47,9 @@ List<SliderModel> getSlide() {
 
   //2
   sliderModel.setImgAssetsPath("assets/bg2.png");
-  sliderModel.setTitle("KuliahOnline");
-  sliderModel.setDesc("loremipsum");
+  sliderModel.setTitle("Kuliah Mudah,\nBahkan Dari Rumah");
+  sliderModel.setDesc(
+      "Daftar kuliah tidak pernah semudah ini, temukan kampus terbaik yang kami sarankan dengan mudah cepat dan tepat !");
 
   slides.add(sliderModel);
 
@@ -55,8 +57,8 @@ List<SliderModel> getSlide() {
 
   //3
   sliderModel.setImgAssetsPath("assets/bg3.png");
-  sliderModel.setTitle("KuliahOnline");
-  sliderModel.setDesc("loremipsum");
+  sliderModel.setTitle("Daftar\nSekarang !");
+  sliderModel.setDesc("");
 
   slides.add(sliderModel);
 
